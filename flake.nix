44a7824e4ed1528f51c17cd405621c25233cadbc
Ludovico Piero <lewdovico@gnuweeb.org>
2025-05-16 12:58:34 +0900

    flake: update flake.lock
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
